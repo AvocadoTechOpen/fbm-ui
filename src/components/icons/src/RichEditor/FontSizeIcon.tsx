3ffa3440565bc66140993117cfd35a1eb6ff0631
wangyu <wy85600@163.com>
2022-03-24 21:12:24 +0800

    Icon

---
+++ src/components/icons/src/RichEditor/FontSizeIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SvgIcon from '@mui/material/SvgIcon';
+
+const FontSizeIcon = (props) => (
+  <SvgIcon {...props}>
+    <path d="M15,7 L15,8 L11,8 L11,18 L10,18 L10,8 L6,8 L6,7 L15,7 Z M19,12 L19,13 L16.5,13 L16.5,18 L15.5,18 L15.5,13 L13,13 L13,12 L19,12 Z" fill="#5C6165"></path>
+  </SvgIcon>
+);
+
+export default FontSizeIcon;
---
+++ src/components/icons/src/RichEditor/RichEditorDropdownIcon.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import SvgIcon from '@mui/material/SvgIcon';
+
+const RichEditorDropdownIcon = (props) => (
+  <SvgIcon {...props}>
+    <polygon id="路径-6" fill="#5C6165" fill-rule="nonzero" transform="translate(3.500000, 11.500000) rotate(-45.000000) translate(-3.500000, -11.500000) " points="2 9 2 13 6 13 6 14 1 14 1 9"></polygon>
+  </SvgIcon>
+);
+
+export default RichEditorDropdownIcon;
