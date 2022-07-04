ef0bd506ab66e613edf26f592ae37db3f92cec00
bxf1234 <xybaoxiangfei@163.com>
2022-07-04 16:14:32 +0800

    fix:starticon

---
+++ src/components/icons/src/StarIcon.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { SvgIcon } from '@mui/material';
+import type { SvgIconProps } from "@mui/material/SvgIcon";
+
+const StarIcon: React.FC<SvgIconProps> = (props) => (
+  <SvgIcon {...props}>
+    <g
+      id="Page-1"
+      stroke="none"
+      stroke-width="1"
+      fill-rule="evenodd"
+    >
+      <g
+        id="🥑招聘流程-列表"
+        transform="translate(-1502.000000, -186.000000)"
+      >
+        <g id="列表展示" transform="translate(841.000000, 34.000000)">
+          <g id="表格" transform="translate(162.000000, 85.000000)">
+            <g id="列2" transform="translate(4.000000, 51.000000)">
+              <g id="star" transform="translate(495.000000, 16.000000)">
+                <polygon
+                  id="Star"
+                  points="9 13.5 3.70993273 16.2811529 4.72024568 10.3905765 0.440491353 6.21884705 6.35496636 5.35942353 9 0 11.6450336 5.35942353 17.5595086 6.21884705 13.2797543 10.3905765 14.2900673 16.2811529"
+                ></polygon>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+  </SvgIcon>
+);
+
+export default StarIcon;
