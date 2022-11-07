dec5d2f98543d08a854682a07cab2fdffb755db6
lizunlong <lizunlong@gmail.com>
2022-11-07 20:56:00 +0800

    fix [Drawer]: when isNullFooter

--- src/components/Drawer/src/Content.tsx
+++ src/components/Drawer/src/Content.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
 import styled from '@mui/material/styles/styled'
 
 import Box from '../../Box'
 
-const DrawerContent: React.FC = styled(Box)({
-  height: 'calc(100vh - 56px)',
+const DrawerContent = styled(Box)(({ isNullFooter = false }: { isNullFooter?: boolean }) => ({
+  height: isNullFooter ? 'unset' : 'calc(100vh - 56px)',
   overflowY: 'auto',
   padding: '0 16px'
-})
+}))
 
 export default DrawerContent
