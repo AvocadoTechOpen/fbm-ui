b404f9b2d1662f7fb922d011db58be5206b7d519
lizunlong <lizunlong@gmail.com>
2022-12-09 15:09:41 +0800

    fix: Checkbox

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import { FormControlLabel as MuiFormControlLabel, formControlLabelClasses, styled, FormControlLabelProps as MuiFormControlLabelProps, Checkbox as MuiCheckbox, CheckboxProps as MuiCheckboxProps, createChainedFunction, checkboxClasses } from '@mui/material';
+import React from 'react'
+import {
+  FormControlLabel as MuiFormControlLabel,
+  styled,
+  FormControlLabelProps as MuiFormControlLabelProps,
+  Checkbox as MuiCheckbox,
+  CheckboxProps as MuiCheckboxProps,
+  createChainedFunction,
+  checkboxClasses,
+} from '@mui/material'
 import useCheckboxGroup from '../CheckboxGroup/useCheckboxGroup'
 
 export interface CheckboxProps {
-  ref?: MuiCheckboxProps['ref'];
-  label?: string | number | React.ReactElement;
-  value?: unknown;
-  disabled?: boolean;
-  name?: string;
-  checked?: boolean;
-  onChange?: (event: React.SyntheticEvent, checked: boolean) => void;
-  FormControlLabelProps?: MuiFormControlLabelProps;
-  CheckboxProps?: MuiCheckboxProps;
+  ref?: MuiCheckboxProps['ref']
+  label?: string | number | React.ReactElement
+  value?: unknown
+  disabled?: boolean
+  name?: string
+  checked?: boolean
+  onChange?: (event: React.SyntheticEvent, checked: boolean) => void
+  FormControlLabelProps?: MuiFormControlLabelProps
+  CheckboxProps?: MuiCheckboxProps
+  sx?: any;
 }
 
 function areEqualValues(values, name): boolean {
@@ -20,6 +29,7 @@ function areEqualValues(values, name): boolean {
 
 const Checkbox = styled(MuiCheckbox)(({ checked }) => {
   return {
+    color: 'rgba(0,0,0,.26)',
     [`&.${checkboxClasses.root}`]: {
       width: 36,
       height: 36,
@@ -27,64 +37,75 @@ const Checkbox = styled(MuiCheckbox)(({ checked }) => {
         background: 'rgba(76, 175, 80, .08)',
       },
     },
-    color: 'rgba(0,0,0,.26)',
     [`&.${checkboxClasses.disabled}`]: {
       color: 'rgba(0,0,0,.12)',
       ...(checked && {
         color: 'rgba(0,0,0,.26)',
-      })
-    }
-  }  
+      }),
+    },
+  }
 })
 
+const FormControlLabelAndCheckbox: React.FC<CheckboxProps> = React.forwardRef(
+  (props, ref) => {
+    const {
+      label,
+      value,
+      disabled,
+      name: nameProp,
+      checked: checkedProp,
+      onChange: onChangeProp,
+      FormControlLabelProps,
+      CheckboxProps,
+      sx,
+      ...otherProps
+    } = props
 
-const FormControlLabelAndCheckbox: React.FC<CheckboxProps> = React.forwardRef((props, ref) => {
-  const {
-    label,
-    value,
-    disabled,
-    name: nameProp,
-    checked: checkedProp,
-    onChange: onChangeProp,
-    FormControlLabelProps,
-    CheckboxProps,
-    ...otherProps
-  } = props
+    const checkboxGroup = useCheckboxGroup()
 
-  const checkboxGroup = useCheckboxGroup();
+    let checked = checkedProp
+    const onChange = createChainedFunction(
+      onChangeProp,
+      checkboxGroup && checkboxGroup.onChange
+    )
+    let name = nameProp
 
-  let checked = checkedProp;
-  const onChange = createChainedFunction(onChangeProp, checkboxGroup && checkboxGroup.onChange);
-  let name = nameProp;
-
-  if (checkboxGroup) {
-    if (typeof nameProp === 'undefined') {
-      name = checkboxGroup.name;
+    if (checkboxGroup) {
+      if (typeof nameProp === 'undefined') {
+        name = checkboxGroup.name
+      }
+      if (typeof checked === 'undefined') {
+        checked = areEqualValues(checkboxGroup.value, value)
+      }
     }
-    if (typeof checked === 'undefined') {
-      checked = areEqualValues(checkboxGroup.value, value);
+
+    // 兼容formItem
+    if (typeof checked === 'undefined' && typeof value === 'boolean') {
+      checked = value
     }
-  }
 
-  // 兼容formItem
-  if (typeof checked === 'undefined' && typeof value === 'boolean') {
-    checked = value
+    return (
+      <MuiFormControlLabel
+        name={name}
+        value={value}
+        onChange={onChange}
+        label={label}
+        checked={checked}
+        disabled={disabled}
+        control={<Checkbox ref={ref} {...CheckboxProps} />}
+        {...FormControlLabelProps}
+        sx={{
+          ...sx,
+          '& .MuiFormControlLabel-label': {
+            fontSize: '14px',
+            color: 'rgba(0,0,0,0.86)',
+          },
+        }}
+        {...otherProps}
+      />
+    )
   }
-
-  return (
-    <MuiFormControlLabel
-      name={name}
-      value={value}
-      onChange={onChange}
-      label={label}
-      checked={checked}
-      disabled={disabled}
-      control={<Checkbox ref={ref} {...CheckboxProps} />}
-      {...FormControlLabelProps}
-      {...otherProps}
-    />
-  )
-})
+)
 
 FormControlLabelAndCheckbox.defaultProps = {
   label: '',
@@ -92,4 +113,4 @@ FormControlLabelAndCheckbox.defaultProps = {
 
 export { Checkbox }
 
-export default FormControlLabelAndCheckbox;
+export default FormControlLabelAndCheckbox
